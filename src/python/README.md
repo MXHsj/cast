2741a40e29804af460957df44255230353f5c61c
Ma Xihan <31639301+MXHsj@users.noreply.github.com>
2022-04-11 15:52:24 -0400

    working on Windows10

--- src/python/README.md
+++ src/python/README.md
@@ -11,3 +11,9 @@ Executing under Linux:
 - Install Pillow (latest PIL library) and PySide2 using pip
 - Copy pycast.so and libcast.so to {cast_api_path}/src/python
 - Execute: LD_LIBRARY_PATH=. python3 {clarius_python_example}.py
+
+Executing under Windows10 (Python3.8, not working properly)
+- Copy cast.dll, cast.lib and pycast.pyd to {cast_api_path}/src/python
+- Turn on Mobile hotspot: settings -> Network & Internet -> Mobile hotspot
+- Connect mobile device and Clarius probe to Windows Mobile hotspot
+- Execute: python {clarius_python_example}.py --address $ip$ -- port $port$
--- src/python/pycaster_mod.py
+++ src/python/pycaster_mod.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 '''
-publish US image stream to ROS topic
+publish US image stream using OpenCV
 '''
 import pycast
 import argparse
+import numpy as np
 from cv2 import cv2
 from PySide2 import QtCore, QtGui, QtWidgets
-import rospy
-import numpy as np
-from cv_bridge import CvBridge, CvBridgeError
-from sensor_msgs.msg import Image
+from PySide2.QtCore import Qt, Slot, Signal
 
-
+global processed
 processed = None
-
 
 # called when a new processed image is streamed
 # @param image the scan-converted image data
@@ -23,14 +20,14 @@ processed = None
 # @param micronsPerPixel microns per pixel
 # @param timestamp the image timestamp in nanoseconds
 def newProcessedImage(image, width, height, bpp, micronsPerPixel, timestamp, imu):
-  global processed
-  print("new image (sc): {0}, {1}x{2} @ {3} bpp, {4:.2f} um/px, imu: {5} pts".
-        format(timestamp, width, height, bpp, micronsPerPixel, len(imu)))
-  img = QtGui.QImage(image, width, height, QtGui.QImage.Format_ARGB32)
-  # img.save('processed.jpg')
-  ptr = img.constBits()
-  processed = np.array(ptr).reshape(height, width, 4)  # Copies the data
-  return
+	global processed
+	print("new image (sc): {0}, {1}x{2} @ {3} bpp, {4:.2f} um/px, imu: {5} pts".
+				format(timestamp, width, height, bpp, micronsPerPixel, len(imu)))
+	img = QtGui.QImage(image, width, height, QtGui.QImage.Format_ARGB32)
+	# img.save('processed.jpg')
+	ptr = img.constBits()
+	processed = np.array(ptr).reshape(height, width, 4)  # Copies the data
+	return
 
 
 # called when a new raw image is streamed
@@ -43,7 +40,7 @@ def newProcessedImage(image, width, height, bpp, micronsPerPixel, timestamp, imu
 # @param timestamp the image timestamp in nanoseconds
 # @param jpg jpeg compression size if the data is in jpeg format
 def newRawImage(image, lines, samples, bps, axial, lateral, timestamp, jpg):
-  return
+	return
 
 
 # called when a new spectrum image is streamed
@@ -56,82 +53,91 @@ def newRawImage(image, lines, samples, bps, axial, lateral, timestamp, jpg):
 # @param velocityPerSample velocity per sample for a pw spectrum
 # @param pw flag that is true for a pw spectrum, false for an m spectrum
 def newSpectrumImage(image, lines, samples, bps, period, micronsPerSample, velocityPerSample, pw):
-  return
+	return
 
 
 # called when freeze state changes
 # @param frozen the freeze state
 def freezeFn(frozen):
-  if frozen:
-    print("imaging frozen")
-  else:
-    print("imaging running")
-  return
+	if frozen:
+			print("imaging frozen")
+	else:
+			print("imaging running")
+	return
 
 
 # called when a button is pressed
 # @param button the button that was pressed
 # @param clicks number of clicks performed
 def buttonsFn(button, clicks):
-  print("button pressed: {0}, clicks: {1}".format(button, clicks))
-  return
+	print("button pressed: {0}, clicks: {1}".format(button, clicks))
+	return
 
 
 # main function
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--address', '-a', dest='ip', help='ip address of probe.', required=True)
-  parser.add_argument('--port', '-p', dest='port', type=int, help='port of the probe', required=True)
-  parser.add_argument('--width', '-w', dest='width', type=int, help='image output width in pixels')
-  parser.add_argument('--height', '-ht', dest='height', type=int, help='image output height in pixels')
-  parser.set_defaults(ip=None)
-  parser.set_defaults(port=None)
-  parser.set_defaults(width=640)
-  parser.set_defaults(height=480)
-  args = parser.parse_args()
+	global processed
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--address', '-a', dest='ip',
+											help='ip address of probe.', required=True)
+	parser.add_argument('--port', '-p', dest='port', type=int,
+											help='port of the probe', required=True)
+	parser.add_argument('--width', '-w', dest='width',
+											type=int, help='image output width in pixels')
+	parser.add_argument('--height', '-ht', dest='height',
+											type=int, help='image output height in pixels')
+	parser.set_defaults(ip=None)
+	parser.set_defaults(port=None)
+	parser.set_defaults(width=640)
+	parser.set_defaults(height=480)
+	args = parser.parse_args()
 
-  if not args.ip or not args.port or args.port < 0:
-    print("one or more arguments are invalid")
-    parser.print_usage()
-    return
+	if not args.ip or not args.port or args.port < 0:
+		print("one or more arguments are invalid")
+		parser.print_usage()
+		return
 
-  path = './'
+	path = './'
 
-  # initialize
-  cast = pycast.Caster(newProcessedImage, newRawImage, newSpectrumImage, freezeFn, buttonsFn)
-  ret = cast.init(path, args.width, args.height)
-  if ret:
-    print("initialization succeeded")
-    ret = cast.connect(args.ip, args.port)
-    if ret:
-      print("connected to {0} on port {1}".format(args.ip, args.port))
-    else:
-      print("connection failed")
-      cast.destroy()
-      return
-  else:
-    print("initialization failed")
-    return
-  rospy.init_node('ClariusPublisher', anonymous=True)
-  US_pub = rospy.Publisher('Clarius/US', Image, queue_size=1)
+	# initialize
+	cast = pycast.Caster(newProcessedImage, newRawImage,
+												newSpectrumImage, freezeFn, buttonsFn)
+	ret = cast.init(path, args.width, args.height)
+	if ret:
+		print("initialization succeeded")
+		ret = cast.connect(args.ip, args.port)
+		if ret:
+			print("connected to {0} on port {1}".format(args.ip, args.port))
+		else:
+			print("connection failed")
+			cast.destroy()
+			return
+	else:
+		print("initialization failed")
+		return
 
-  # loop
-  isVisualize = True
-  try:
-    while not rospy.is_shutdown():
-      if processed is not None:
-        US_pub.publish(CvBridge().cv2_to_imgmsg(processed, encoding="passthrough"))
-        if isVisualize:
-          cv2.imshow('processed image', processed)
-      key = cv2.waitKey(10)
-      if key == ord('q'):
-        break
-  except KeyboardInterrupt:
-    print('terminated by user')
+	# loop
+	cv2.namedWindow('clarius',cv2.WINDOW_AUTOSIZE)
+	isVisualize = True
+	try:
+		while True:
+			if processed is not None:
+				if isVisualize:
+					cv2.imshow('clarius', processed)
+			key = cv2.waitKey(10)
+			if key == ord('q'):
+				break
+			elif key == ord('s'):
+				if processed is not None:
+					print('save image')
+					cv2.imwrite('./BScan.jpg', processed)
+				else:
+					print('empty image')
+	except KeyboardInterrupt:
+		print('terminated by user')
 
-  cast.destroy()
-  cv2.destroyAllWindows()
-
+	cv2.destroyAllWindows()
+	cast.destroy()
 
 if __name__ == '__main__':
-  main()
+	main()
